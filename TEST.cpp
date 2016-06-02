f86344f45ca655b6375b64aa4be8d5e73c3ee23d
YenBoChen <t26572435@gmail.com>
2016-06-02 22:57:56 +0800

    TEST commit

---
+++ TEST.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+        
+int main(void)
+{
+
+    printf("Hello1\n");
+    printf("Hello2\n");   
+                
+    system("pause");
+    return 0;
+}
