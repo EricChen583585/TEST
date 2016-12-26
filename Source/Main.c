bbb89c80eff9e45c89df624754a862c73936eaeb
YenBoChen <t26572435@gmail.com>
2016-12-26 21:09:30 +0800

    First commit
    [IMPACT PROJECTS]
    Source/Main.c
    Hearder/Main.h
    
    [DESCRIPTION]
    1.First commit and print message
    
    [TEST]
    build pass and test pass

---
+++ Source/Main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#include "D:/Git_Repositary/Hearder/Main.h"
+
+
+int main(){
+    
+    printf("Hi Eric\n");
+    Show();
+    system("pause");
+    return;    
+}
+void Show(){
+     
+     printf("Run Func\n");
+}
