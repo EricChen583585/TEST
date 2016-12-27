6373c920894692a35bb202a60d0d6a04067ab390
YenBoChen <t26572435@gmail.com>
2016-12-28 01:17:25 +0800

    Add the Math function
    [IMPACT PROJECTS]
    Source/Main.c
    Hearder/Math.h
    
    [DESCRIPTION]
    1.Adder
    2.Sub
    3.Multi
    4.Div
    
    [TEST]
    build pass and test pass

---
+++ Hearder/Math.h
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int Adder(int a, int b);
+int Subtractor(int a, int b);
+int Multi(int a, int b);
+int Divider(int a, int b);
--- Source/Main.c
+++ Source/Main.c
@@ -2,17 +2,18 @@
 #include<stdlib.h>
 
 #include "D:/Git_Repositary/Hearder/Main.h"
-
+#include "D:/Git_Repositary/Hearder/Math.h"
 
 int main(){
     
     printf("Hi Eric\n");
     Show();
     Math_Func(3,5);
-    
+    printf("Adder:%d /Sub:%d /Multi:%d /Div:%d\n",Adder(1,2),Subtractor(6,2),Multi(3,5),Divider(9,3));
     system("pause");
     return;    
 }
+
 void Show(){
      
      printf("Run Func\n");
@@ -23,4 +24,19 @@ int Math_Func(int a,int b){
     c=a+b;
     printf("a:%d / b:%d / c:%d\n",a,b,c);
     return c;
+}
+int Adder(int a, int b){
+    return a+b;
+}
+
+int Subtractor(int a, int b){
+    return a-b;
+}
+
+int Multi(int a, int b){
+    return a*b;
+}
+
+int Divider(int a, int b){
+    return a/b; 
 }
